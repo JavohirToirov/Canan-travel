83056fc5af15d03864067e28172ed022457b5d85
JavohirToirov <javohir1427@gmail.com>
2021-12-31 22:01:51 +0300

    add new page

--- pages/info_tourists.php
+++ pages/info_tourists.php
@@ -8,4 +8,25 @@
                 </div>
             </div>
    </div>
+   
+   <div class="info-tourst-mainBlock">
+      <div class="container">
+          <div class="info-tourst-mainBlock-box">
+            <h2>Visa To Uzbekistan</h2>
+            <p>Nowadays the Republic of Uzbekistan has different diplomatic relations with the countries of the world community. For foreign citizens from all over the world, the way to enter Uzbekistan may differ.</p>
+            <div class="info-tourst-mainBlock-box-reg">
+            <p>There are several regimes to enter the Republic of Uzbekistan:</p>
+              <ul>
+                 <li>Visa-Free</li>
+                 <li>Simplified Visa Issue</li>
+                 <li>E-Visa</li>
+                 <li>Visa with Invitation</li>
+                 <li>Visa on Arrival</li>
+                 <li>Transit Visa</li>
+                 <li>Visa-Free Transit</li>
+              </ul>
+            </div>
+          </div>
+      </div>
+   </div>
 </div>
