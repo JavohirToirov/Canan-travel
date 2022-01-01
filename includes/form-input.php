7fff1b1c30476f5a73ff3bcaddd36aa52f8ce607
JavohirToirov <javohir1427@gmail.com>
2022-01-01 16:52:07 +0300

    pilaf page end

---
+++ includes/form-input.php
@@ -0,0 +1,21 @@
+<div class="pilof-mainBlock-box-form">
+                  <div class="pilof-mainBlock-box-form-box">
+                      <h4>LEAVE A REPLY</h4>
+                      <form action="">
+                        <div class="d-flex align-items-center author-box">
+                           <div class="pilof-mainBlock-box-form-box-author">
+                          <input id="author" name="author" type="text" value=""    placeholder="Name*" size="30" aria-required="true" required>
+                           </div>
+                           <p class="pilof-mainBlock-box-form-box-check">
+                           <input type="checkbox" value="yes" required><label for="wp-comment-cookies-consent">Save my name in this browser for the next time I comment.</label>
+                           </p>
+                        </div>
+                        <div class="pilof-mainBlock-box-form-box-comment">
+                            <textarea name="comment" id="" cols="45" rows="8" placeholder="Comment*" required></textarea>
+                        </div>
+                         <p class="form-submit">
+                           <input type="submit" name="submit" id="submit" value="Post Comment">
+                         </p>
+                     </form>
+                  </div>
+</div>
--- includes/mealsFunction.php
+++ includes/mealsFunction.php
@@ -14,9 +14,25 @@ $arrayPagesmaels = [
         'page_name' => 'round bread',
         'page_title' => 'round bread'
     ],
-    'management' => [
-        'page_name' => 'management',
-        'page_title' => 'management'
+    'sumalak' => [
+        'page_name' => 'sumalak',
+        'page_title' => 'sumalak'
+    ],
+    'shurpa' => [
+        'page_name' => 'shurpa',
+        'page_title' => 'shurpa'
+    ],
+    'manta' => [
+        'page_name' => 'manta',
+        'page_title' => 'manta'
+    ],
+    'tukhum-barak' => [
+        'page_name' => 'tukhum-barak',
+        'page_title' => 'tukhum-barak'
+    ],
+    'tandoor-meat' => [
+        'page_name' => 'tandoor-meat',
+        'page_title' => 'tandoor-meat'
     ],
 ];
 
