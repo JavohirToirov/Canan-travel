3351acc8c61cf433f7beee3d3d818f3eb5eaa0a6
JavohirToirov <javohir1427@gmail.com>
2022-01-01 11:48:18 +0300

    info_tourists page end

--- pages/info_tourists.php
+++ pages/info_tourists.php
@@ -59,6 +59,126 @@
                      <p>Kyrgyzstan</p>
                   </div>
                </div>
+               <p>A visa is issued upon a personal appeal of foreign citizens to the appropriate diplomatic mission or consulate of the Republic of Uzbekistan in their country of residence with the following list of documents:</p>
+               <ul>
+                 <li>electronic visa application form (printed and completed);</li>
+                 <li>original passport (the validity of which must exceed the validity of the visa by at least 3 months);</li>
+                 <li>1 color photograph (passport size);</li>
+                 <li>consular fee (according to the tariffs listed in the Duty section)</li>
+               </ul>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <p>For residents of 50 States ( <strong>the list is provided below</strong> ), a simplified procedure for obtaining visa within <strong>3 working days</strong> is introduced, which does not require an invitation from legal/physical entity from the Republic of Uzbekistan to the Ministry of Foreign Affairs in the country of residence.</p>
+               <p>Visa processing is carried out by personal appeal of foreign citizens to the appropriate <strong>diplomatic mission or Consulate of the Republic of Uzbekistan</strong> in the country of their stay with the following list of documents:</p>
+               <ul>
+                 <li>Electronic visa application form (filled and printed out);</li>
+                 <li>Original passport (the validity of which must exceed the validity of the visa for at least 3 months);</li>
+                 <li>1 color photo (passport size);</li>
+                 <li>Visa fee (according to the tariffs given in Duty section)</li>
+               </ul>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <h2>Visa To Uzbekistan</h2>
+               <p>Residents of <strong>the above States</strong> also have the opportunity to obtain an electronic tourist visa by filling out a questionnaire on the <strong>E-VISA</strong> portal and paying a fee of <strong>20 USD</strong>. E-visa is sent in the PDF format on the e-mail of the applicant and is not pasted into the passport.</p>
+               <ul>
+                 <li>The submitted documents or electronic application are considered within 3 working days.</li>
+                 <li>E-visa is valid for 90 days from the date of issue.</li>
+                 <li>The period of stay in the territory of Uzbekistan with e-visa is 30 days.</li>
+               </ul>
+               <blockquote><p><strong>If the applicant has dual citizenship, the e-visa application form must be filled with the data of the passport on the basis of which it is planned to enter the Republic of Uzbekistan. In case of the passport replacement during the validity period of the e-visa, it is a must to apply for e-visa again.</strong></p></blockquote>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <h2>Visa with Invitation / Visa Support</h2>
+               <p>For citizens of all other countries visa is issued on the basis of <strong>letter of invitation</strong> or <strong>visa support</strong>. For visa support, you need to provide our company with the following list of documents:</p>
+               <ul>
+                 <li>Filled application form for visa support (<a href="https://getfile.dokpub.com/yandex/get/https://yadi.sk/i/WFUWZp4Jx4nrIQ">download</a>)</li>
+                 <li>Scanned copy of Your passport</li>
+                 <li>Scanned copy of letter of employment</li>
+               </ul>
+               <p>After we receive all the necessary information, we will issue a visa support in the Ministry of Foreign Affairs of the Republic of Uzbekistan in Tashkent which will consider Your documents within <strong>7 working days.</strong></p>
+               <blockquote><p>The MFA of the Republic of Uzbekistan reserves the right to refuse to issue a permit without providing reasons for refusal. In case of a refusal, the fee paid will not be refunded.</p></blockquote>
+               <p>Having received permission from the MFA, we will send you a <strong>telex number</strong> by e-mail and then you can contact the <strong>diplomatic mission or Consulate of the Republic of Uzbekistanin</strong> the country of Your stay with a list of the following documents:</p>
+               <ul>
+                 <li>Original passport (the validity of which must exceed the validity of the visa for at least 3 months);</li>
+                 <li>Filled visa application form in 2 copies;</li>
+                 <li>2 color photos (passport size);</li>
+                 <li>Telex number (sent to Your e-mail);</li>
+                 <li>Visa fee (according to the tariffs given in Duty section)</li>
+               </ul>
+               <p>The submitted documents are considered within <strong>3 working days.</strong></p>
+               <blockquote><p>Visa support will be terminated if the client refuses the previously booked package of travel services.</p></blockquote>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <h2>Visa on Arrival</h2>
+               <p>If the country of Your stay does not have <strong>the diplomatic mission or Consulate of the Republic of Uzbekistan</strong>, You will be able to apply for visa on arrival at the international airport named after «Islam Karimov» in the <strong>Visa support sector.</strong></p>
+               <p>To do this, you need to apply for visa support in our company by sending us the following list of documents:</p>
+               <ul>
+                 <li>Filled application form for visa support (<a href="https://getfile.dokpub.com/yandex/get/https://yadi.sk/i/WFUWZp4Jx4nrIQ">download</a>)</li>
+                 <li>Scanned copy of Your passport</li>
+                 <li>Scanned copy of letter of employment</li>
+                 <li>Scanned copies of airtickets or booking</li>
+               </ul>
+               <p>After we receive all the necessary information, we will issue a visa support in the Ministry of Foreign Affairs of the Republic of Uzbekistan in Tashkent which will consider Your documents within <strong>7 working days.</strong></p>
+               <p>Obtaining the permission of the MFA, we will send You e-mail with an official document containing a <strong>telex number</strong>. This document must be presented at the passport control in the country of Your stay and on its basis You are issued a visa on arrival at the international airport named after «Islam Karimov» in the <strong>Visa support sector.</strong></p>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <h2>Transit Visa</h2>
+               <p>Foreign citizens in transit through the Republic of Uzbekistan, to obtain a transit visa for a period of <strong>72 hours</strong>, must submit to <strong>the diplomatic mission or Consulate of the Republic of Uzbekistan</strong> in the country of their stay <strong>the entire list of documents necessary for the tourist visa</strong>, as well as attach to it:</p>
+               <ul>
+                 <li>Visa of the destination country;</li>
+                 <li>Travel documents with confirmed date of departure from Uzbekistan to the destination country;</li>
+               </ul>
+               <p>The submitted documents are considered within <strong>3 working days.</strong></p>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text dropdown">
+               <h2>Visa-Free Transit</h2>
+               <p>For citizens of a number of States ( <strong>the list is given below</strong> ) entering the Republic of Uzbekistan for a period of <strong>up to 5 days</strong> with subsequent departure to a third country ( <strong>subject to their tickets issued through the Uzbekistan Airways both to Uzbekistan and to a third country</strong> ), <strong>short-term visa-free regime</strong> is set.</p>
+               <p><strong>The state of destination may not be the starting state of transit.</strong></p>
+               <p>For citizens of the following countries visa-free transit through Uzbekistan for a period of <strong>not more than 5 days</strong> is available:</p>
+               <div class="dropdown-box">
+                  <h3><i class="fas fa-caret-down"></i>View the list:</h3>
+                  <div class="dropdown-box-content">
+                     <p><strong>Permanent</strong></p>
+                     <p>Azerbaijan Armenia Belarus Georgia Kazakhstan Moldova Russia Ukraine</p>
+                     <p><strong>Entry for a period of 30 days</strong></p>
+                     <p>France Germany Indonesia Israel Japan South</p>
+                     <p>Korea Malaysia Singapore Tajikistan Turkey</p>
+                     <p><strong>Entry for a period of 60 days</strong></p>
+                     <p>Kyrgyzstan</p>
+                  </div>
+               </div>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <h2>Duty</h2>
+               <p>For registration and issuance of visa, the following consular fees are provided:<strong>1. Single entry visa:</strong></p>
+               <ul class="duty">
+                 <li>up to 7 days — 40 USD;</li>
+                 <li>up to 15 days — 50 USD;</li>
+                 <li>up to 30 days — 60 USD;</li>
+                 <li>up to 3 months — 80 USD;</li>
+                 <li>up to 6 months — 120 USD;</li>
+                 <li>up to 1 year — 160 USD.</li>
+               </ul>
+               <p><strong>For each additional entry the tariff increases by 10 USD.2. Multiple entry visa:</strong></p>
+               <ul class="duty">
+                 <li>up to 6 months — 150 USD;</li>
+                 <li>up to 1 year — 250 USD.</li>
+               </ul>
+               <p><strong>3. Transit visa:</strong></p>
+               <ul class="duty">
+                 <li>up to 72 hours — 40 USD;</li>
+                 <li>for double-entry transit visa — 50 USD.</li>
+               </ul>
+               <p><strong>4. Group visa</strong> (at least 5 people, excluding children under 16):</p>
+               <ul class="duty">
+                 <li>up to 15 days — 15 USD per person;</li>
+                 <li>up to 30 days — 25 USD per person.</li>
+               </ul>
+               <blockquote><p><strong>When applying for a visa, it is also a must to pay the actual costs. The amount of such costs depends on the place of visa application and does not depend on the citizenship of the applicant.</strong></p></blockquote>
+            </div>
+            <div class="info-tourst-mainBlock-box-reg-text">
+               <h2>PAY ATTENTION</h2>
+               <blockquote><p>For foreign citizens who have not reached <strong>16 years of age</strong>, entry into the Republic of Uzbekistan does not require a visa and is carried out on their own passport for the duration of the visa of the accompanying person, but <strong>no more than 90 days</strong> from the date of entry into the country. If the child is traveling with one of the parents, it is desirable to have a notarized permission to leave the child’s home country from the second parent to prevent possible misunderstandings at the airport of departure.</p></blockquote>
             </div>
             </div>
           </div>
