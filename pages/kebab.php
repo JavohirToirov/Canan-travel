36f4eeb9c2c2bb078d9ae5200685506650b06502
JavohirToirov <javohir1427@gmail.com>
2022-01-02 19:23:36 +0300

    kebab page end

---
+++ pages/kebab.php
@@ -0,0 +1,123 @@
+<div class="main-block">
+   <div class="header-fon about-header-fon pilof-backg">
+            <div class="container">
+                <div class="header-fon-text">
+                    <div class="header-fon-text-box">
+                        <h3 class="header-fon-text-box-h3 history-h3">Uzbek kebab</h3>
+                    </div>
+                </div>
+            </div>
+   </div>
+    
+    <div class="kebab-mainBlock">
+        <div class="kebab-mainBlock-box">
+            <div class="kebab-mainBlock-box-img">
+                    <img lazy="loading" src="/image/blog_page/meals/uzbekskiy-shashlik.jpg" alt="">
+            </div>
+            <div class="kebab-mainBlock-box-item">
+                    <p>
+                        Kebab is one of the oldest dishes in the world. After all, once people killed mammoths and roasted meat on the fire. The dish is known all over the world and in each country is prepared according to a unique recipe.
+                    </p>
+                    <p>
+                        Let’s understand what is special about Uzbek barbecue and how to cook it.
+                    </p>
+                    <p>
+                        <strong>What is the difference between Uzbek shashlik and dishes of other countries?</strong>
+                        <br>According to Uzbek national cuisine, shashlik is made from lamb. But if you have beef, pork or chicken at hand — you can experiment. The correct name of the dish is “kabob”, but more often it is called “kebab”.
+                    </p>
+                    <p>
+                      Kebabs are prepared only by men, and women do not trust such work. First of all, the masters burn the fire for about 40 minutes, until a layer of gray ash appears and only then prepare the dish. Previously, as firewood was used branches of saxaul (a desert is a small tree originally from Central Asia), but today most chefs use conventional charcoal. It is not recommended to use coniferous wood: they can spoil the taste of meat.
+                    </p>
+                    <p>
+                        <strong>Features of the marinade</strong>
+                        Just take the meat of a young lamb and fry it — half the battle. One of the main components in the preparation of kebab — especially marinade. In Uzbekistan, Marinovka takes place in ordinary water. And this is not a typo: indeed, the meat is marinated in ordinary drinking water, sometimes — in mineral water. In a small amount of water add salt, pepper, spices (red pepper, wine vinegar, lemon juice, coriander) and sliced onion rings.
+                    </p>
+                    <p>
+                      <img lazy="loading" width="100%" src="/image/blog_page/meals/marinad-dlya-shashlika-300x167.jpg" alt="">
+                    </p>
+                    <p>
+                       The marinade is a real field for experiments. You can add absolutely any spices, but the main thing — do not overdo it.
+                    </p>
+                    <p>
+                       The taste of Uzbek kebab depends on three factors: meat, marinade and skill of the cook. This master closely monitors the process, does not allow the meat to burn and extinguishes the open flame in time
+                    </p>
+                    <p>
+                        <strong>Types of Uzbek kebab</strong>
+                        Masters prepare up to 20 varieties of barbecue. If you go on a tour to Uzbekistan for the may holidays, be sure to try the Uzbek barbecue:
+                    </p>
+                    <ul class="kebab-item-ingridents">
+                        <li>kiyma kabob (skewers of ground meat);</li>
+                        <li>jigar kabob (liver kebab);</li>
+                        <li>carve kabob (the meat is covered with interior fat).</li>
+                    </ul>
+                    <p>
+                       It is also worth trying a local delicacy — skewers of quail. The cooks themselves also like to experiment, so they use not only young lamb, but also chicken, veal, pork, and even fish.
+                    </p>
+                    <p>
+                      Where to try the best barbecue in Uzbekistan
+                      <br>
+                      There is a small town in Bukhara region — Gijduvan. It is not difficult to get here: the city is located 60 km from Bukhara. It is here that the best Uzbek barbecue is prepared.
+                    </p>
+                    <p>
+                       Be sure to go there and try the local barbecue, but do not even try to learn the recipe for cooking. No, the local chefs are friendly: they will be long and painful to tell the details of the marinade and barbecue.
+                    </p>
+                    <p>
+                       <img lazy="loading" width="100%" src="/image/blog_page/meals/prigotovleniye-shashlikov-na-mangale-300x167.jpg" alt="">
+                    </p>
+                    <p>
+                        You even think that you understand everything and are ready to cook a dish on their own, but it is not so. Shish kebab still gets another taste, so do not waste time on studying the recipe. Just try the barbecue, and free time is better to pay local attractions.
+                    </p>
+                    <p>
+                       How to cook kebab “Three fingers”
+                       <br>
+                       Do not worry, when cooking fingers are not used. The recipe got its name because the meat is strung on three skewers. It’s an ancient recipe, by the way. Not to carry in the field, skewers, Uzbek herdsmen strung the meat on the fork and roasted it.
+                    </p>
+                    <p>
+                       You can prepare a dish of lamb ribs or beef pulp. To cook a barbecue for 5 people, you will need:
+                    </p>
+                    <ul class="kebab-item-ingridents">
+                        <li>1-1.5 kg of meat;</li>
+                        <li>300 gr of broadtail fat;</li>
+                        <li>300 gr onion;</li>
+                        <li>5 gr coriander;</li>
+                        <li>100 ml of purified water;</li>
+                        <li>25 g salt;</li>
+                        <li>3 gr ground pepper.</li>
+                    </ul>
+                    <p>
+                       The first step is to cut off the meat veins, cut it into slices across the fibers. Next, the meat is slightly beaten off and put in a bowl (in which there is a marinade).
+                    </p>
+                    <p>
+                       The next step is to prepare the marinating. To do this, grind the spices and salt in a mortar, cut the onion. All this is added to the dishes to the meat and pour carbonated water. Leave the meat in the marinade for an hour.
+                    </p>
+                    <p>
+                       <img lazy="loading" width="100%" src="/image/blog_page/meals/shashlik-tri-palca-300x167.jpg" alt="">
+                    </p>
+                    <p>
+                       Now cut the fat tail, and begin to string it on a skewer, alternating with pieces of meat. It remains to light a fire and fry the meat at an average temperature.
+                    </p>
+                    <p>
+                      Beef kebab
+                       <br>
+                      Skewers of beef tenderloin turn soft and quickly prepared. First of all, the taste of barbecue depends on the freshness of meat, so do not be lazy and go to the market for fresh.
+                    </p>
+                    <p>
+                       To cook a barbecue, you will need:
+                    </p>
+                    <ul class="kebab-item-ingridents">
+                        <li>1 kg clippings;</li>
+                        <li>400 gr onion;</li>
+                        <li>5 gr coriander;</li>
+                        <li>20 g salt;</li>
+                        <li>5-10 gr ground pepper.</li>
+                    </ul>
+                    <p>
+                       <img lazy="loading" width="100%" src="/image/blog_page/meals/podacha-uzbekskogo-shashlika-300x167.jpg" alt="">
+                    </p>
+                    <p>First of all, cut the meat and fat tail. Next, prepare the marinade: onions cut into half rings, grind spices and salt in a mortar. Place the meat, spices and onion dishes, mix, cover and leave for 1-2 hours.</p>
+                    <p>That the meat was not dry, the marinade is to add 100 ml of water and gently stir contents. String meat on skewers and fry it after it has absorbed water. That the meat was not bitter, preferably after marinating to separate from the meat onions.</p>
+            </div>
+            <? include_once './includes/form-input.php'; ?>
+        </div>
+    </div>
+</div>
