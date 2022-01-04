a4d0f1d373a08edaf48976702c4bbd39c43f67c6
JavohirToirov <javohir1427@gmail.com>
2022-01-04 16:48:09 +0300

    round-bread page start

---
+++ pages/round-bread.php
@@ -0,0 +1,41 @@
+<div class="main-block">
+   <div class="header-fon about-header-fon pilof-backg">
+            <div class="container">
+                <div class="header-fon-text">
+                    <div class="header-fon-text-box">
+                        <h3 class="header-fon-text-box-h3 history-h3">Samarkand flat cakes</h3>
+                    </div>
+                </div>
+            </div>
+   </div>
+   
+   <div class="pilof-mainBlock">
+       <div class="container">
+            <div class="pilof-mainBlock-box">
+              <div class="pilof-mainBlock-box-item">
+                   <div class="pilof-mainBlock-box-item-img">
+                     <img src="../image/blog_page/meals/znamenitiy-uzbekskiy-plov.jpg" alt="">
+                   </div>
+                   <div class="pilof-mainBlock-box-item-text">
+                     <p>The main dish in Uzbek cuisine is Pilaf. This dish is always on the table for all solemn occasion, it serves for guests on the anniversary, and wedding events. Pilaf prepares in many Eastern countries. There are many legends telling about the health and nutritional features of pilaf. According to Uzbek pilaf called “osh palov” In the old days it also was the main dish, and in some houses eat it every day. Poor people also enjoyed the pilaf, but only on holidays. It is believed that true festive pilaf could make only men. Often in the teahouses (cafes) are contest for the preparation of this dish.</p>
+                     <p>In the Uzbek cuisine, there are more than fifty varieties of pilaf: meat, chicken, steamed mutton fat-tailed and fat, with peas, with quince, raisins, and potatoes. In X – XII century appear the recipe of pilaf with partridge, which is prepared still nowadays. Many recipes of pilaf for about a thousand years.</p>
+                   </div>
+              </div>
+              <div class="pilof-mainBlock-box-item">
+                   <div class="pilof-mainBlock-box-item-text">
+                     <p>The Cook defines full readiness of dish, striking the boiler skimmer. If the pilaf is not ready you will hear a hissing sound, if it is ready – you will hear the clunk. To the table, pilaf served in lagans – a large ceramic or porcelain dishes. In the old days during the wedding pilaf was allowed to guests in bread (lepeshki).</p>
+                     <p>In Uzbekistan, by the tradition pilaf mostly eat for dinner. Salads of fresh vegetables, such as tomatoes and cucumbers, radishes and onions always complement this dish. This vegetable helps to better assimilate this quite greasy dish. Of course, it is impossible to imagine pilaf without tea. In Tashkent, people prefer to drink black tea, and in other regions of the country give preference to green tea. And to the tea, the hostess will give a crystalline sugar Navad and various sweets.</p>
+                   </div>
+                   <div class="pilof-mainBlock-box-item-img">
+                     <img src="../image/blog_page/meals/uzbekskiy-plov-na-stole-300x200.jpg.png" alt="">
+                   </div>
+              </div>
+              <div class="pilof-mainBlock-box-video">
+                 <iframe width="100%" height="625" src="https://www.youtube.com/embed/WmJQkUiupKk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <p>If you want to know better the Uzbek people and Uzbek traditions, it should be better just enjoy the main dish of Uzbek cuisine – pilaf!</p>
+              </div>
+              <? include_once './includes/form-input.php'; ?>
+            </div>
+       </div>
+   </div>
+</div>
