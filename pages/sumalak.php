045a2ef4673dcaa1a46e6711aebdcc9e767ba1c0
JavohirToirov <javohir1427@gmail.com>
2022-01-05 17:02:44 +0300

    sumalak page start

---
+++ pages/sumalak.php
@@ -0,0 +1,21 @@
+<div class="main-block">
+   <div class="header-fon about-header-fon pilof-backg">
+            <div class="container">
+                <div class="header-fon-text">
+                    <div class="header-fon-text-box">
+                        <h3 class="header-fon-text-box-h3 history-h3">Sumalak — festiv Uzbek delicacy</h3>
+                    </div>
+                </div>
+            </div>
+   </div>
+    
+    <div class="kebab-mainBlock">
+        <div class="kebab-mainBlock-box">
+            <div class="kebab-mainBlock-box-img">
+                    <img lazy="loading" src="/image/blog_page/meals/uzbekskiy-shashlik.jpg" alt="">
+            </div>
+            
+            <? include_once './includes/form-input.php'; ?>
+        </div>
+    </div>
+</div>
