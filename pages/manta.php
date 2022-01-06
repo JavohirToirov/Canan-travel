f4d4b40d522b0308619f0e8d26b772855947d730
JavohirToirov <javohir1427@gmail.com>
2022-01-06 09:43:06 +0300

    manti page start

---
+++ pages/manta.php
@@ -0,0 +1,98 @@
+<div class="main-block">
+   <div class="header-fon about-header-fon pilof-backg">
+            <div class="container">
+                <div class="header-fon-text">
+                    <div class="header-fon-text-box">
+                        <h3 class="header-fon-text-box-h3 history-h3">Uzbek manta</h3>
+                    </div>
+                </div>
+            </div>
+   </div>
+    
+    <div class="kebab-mainBlock">
+        <div class="kebab-mainBlock-box">
+            <div class="kebab-mainBlock-box-img">
+                    <img lazy="loading" src="/image/blog_page/meals/shurpa1.jpg" alt="">
+            </div>
+            <div class="sumalak-main-box-item">
+               <p>
+                  As soon not call this rich soup: chorpa, sopra… but the point is the name of the soup. This dish is prepared from 4 components: meat (lamb), vegetables, fresh herbs, and spices.
+               </p>
+               <p>
+                 Soup is universal. It is served to relatives, friends and during special events.
+               </p>
+               <h3>The history of the soup</h3>
+               <p>
+                  Once upon a time, in Central Asia lived a boy. To his happiness or misfortune, he was poor, but with a good soul. He fell in love with the most beautiful girl in the country, and her father did not even want to listen to the poor groom.
+               </p>
+               <p>
+                  Once the guy made soup from what was — onions, a piece of meat, greens and potatoes. It so happened that the father of the beloved passed by, heard a beautiful aroma and decided to drive closer…
+               </p>
+               <p>
+                   The guy was not angry with his beloved father and offered him to taste his dish. The questions “what is this delicious soup” and “who came up with the recipe” the guy said that he just cooked soup from what was. The father appreciated the intelligence of the guy and gave him his daughter to wife, but on condition…that he would teach him to make the same soup.
+               </p>
+               <p>
+                  In fact, this story is fiction. Shurpa is so ancient dish that it is difficult to say exactly when it was prepared for the first time.
+               </p>
+               <h3>Useful property</h3>
+               <p>
+                   This is not a reservation: shurpa really has useful properties. The doctors of Central Asia recommend eating the soup with anemia, joint problems, inflammation of the lungs, tuberculosis.
+               </p>
+               <p>
+                  <img lazy="loading" src="/image/blog_page/meals/shurpa2.jpg" alt="">
+               </p>
+               <p>
+                  Also shurpa stimulates potency, relieves tension, improves the digestive system and contributes to the appearance of milk in nursing mothers. The dish is so useful that Avicenna (scientist and doctor of Central Asia) wrote about it in his treatise “Canon of medical science”.
+               </p>
+               <h3>Varieties of soup</h3>
+               <p>
+                 Of course, each owner has a unique recipe for cooking shurpa. Some add to the shurpa “special” ingredients, others — prepare a dish with unusual spices. In General, there are 2 types of shurpa:
+               </p>
+               <ul class="sumalak-ingredients">
+                   <li>kaurma. Meat for soup is first fried in a cauldron, and then water and spices are added. Mandatory spice — nut nut (1 kg of meat must be added 200 grams of nut);</li>
+                   <li>kainatma. The meat is not fried in advance: first, the meat is soaked in water for about 1.5-2 hours, and then all the contents are placed in a cauldron and cooked over low heat.</li>
+               </ul>
+               <h3>How to cook the soup</h3>
+               <p>
+                  The soup is easy to prepare both at home and in nature. Recipe shurpa is not difficult, so prepare a shopping list to go to the store.
+               </p>
+               <p>
+                  To prepare the soup, you will need:
+               </p>
+               <ul class="sumalak-ingredients">
+                   <li>1.5-2 kg of lamb (you can use pork or beef, but to feel the taste of this shurpa — need lamb);</li>
+                   <li>3.5-4 liters of water;</li>
+                   <li>4 carrots;</li>
+                   <li>4 onions;</li>
+                   <li>2-2. 5 tablespoon of salt;</li>
+                   <li>seasoning for soup;</li>
+                   <li>1,5-2 kg of pepper;</li>
+                   <li>bunch of coriander and parsley.</li>
+               </ul>
+               <p>
+                 The first step is the preparation of meat. Wash it, wait until the water flows down or wipe it and start to cut into large pieces. Do not cut too finely: each piece should weigh 70-100 grams. It is desirable to buy young meat. Old lamb will cook longer and the taste of the dish will change.
+               </p>
+               <p>
+                 When buying meat, ask the seller what part you are buying. This is important because the taste of the soup depends on the type of meat. Ideally, the buy back of the neck or shoulder blades of females.
+               </p>
+               <p>
+                  Next, make a fire and put it on the cauldron. Pour water into it, immediately throw the sliced pieces of meat. While the water in the cauldron boils, prepare other ingredients: onions clean and cut into half rings, carrots — cut into large pieces.
+               </p>
+               <p>
+                  <img lazy="loading" src="/image/blog_page/meals/shurpa3.jpg" alt=""> 
+               </p>
+               <p>
+                  When the broth begins to boil, foam is formed. It must be removed and thrown into the cauldron carrots and onions. How much further cooking — it’s hard to say. Be guided by the freshness of meat: if you managed to get young meat — cook for 40-60 minutes, if the old — cook more and try the soup to taste.
+               </p>
+               <p>
+                  And of course, add spices. First, add salt to the cauldron and taste. Next, add the seasoning for shurpa, a few tomatoes, sweet peppers and stir the broth. If you do not have a special seasoning for shurpa — do not despair, you can make it yourself. You need to mix coriander, khmeli-suneli, cumin and savory.
+               </p>
+               <p>
+                 Now add the potatoes and bake for another 20 minutes. Next, pour the shurpa on the jambs and add fresh herbs. Yes, to feel the taste of this Uzbek dish — eat it hot. So you will be able to hear the unusual touch of fire and spice.
+               </p>
+            </div>
+            
+            <? include_once './includes/form-input.php'; ?>
+        </div>
+    </div>
+</div>
