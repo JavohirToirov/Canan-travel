7e0930633f18510e8be7a2da3d73e181980b3733
JavohirToirov <javohir1427@gmail.com>
2022-01-06 10:18:30 +0300

    tuxum-barak page start

---
+++ pages/tukhum-barak.php
@@ -0,0 +1,124 @@
+<div class="main-block">
+   <div class="header-fon about-header-fon pilof-backg">
+            <div class="container">
+                <div class="header-fon-text">
+                    <div class="header-fon-text-box">
+                        <h3 class="header-fon-text-box-h3 history-h3">Tukhum-barak national dish</h3>
+                    </div>
+                </div>
+            </div>
+   </div>
+    
+    <div class="kebab-mainBlock">
+        <div class="kebab-mainBlock-box">
+            <div class="kebab-mainBlock-box-img">
+                    <img lazy="loading" src="/image/blog_page/meals/manti1.jpg" alt="">
+            </div>
+            <div class="sumalak-main-box-item">
+               <p>
+                  Manta is one of the main dishes of the Uzbek people, which is cooked on steam. To describe the dish, in a nutshell, is as follows: tender meat filling covered with thin dough.
+               </p>
+               <p>
+                 Well, let’s figure out when there was a dish and how to cook it yourself.
+               </p>
+               <h3>History of manta</h3>
+               <p>
+                  Mantas originally were not Uzbek. For the first time, the dish was prepared by Chinese nomads, and only after some time mantas appeared in Central Asia and other countries.
+               </p>
+               <p>
+                 It is difficult to determine the year of origin of mantas. It happened somewhere in the twentieth century. The recipe changed many times, new ingredients and cooking features were added…and only the people of Uzbekistan have kept the real recipe of the dish to this day.
+               </p>
+               <p>
+                  So, today we can say that manta is the national dish of Uzbekistan.
+               </p>
+               <h3>When is manta served?</h3>
+               <p>
+                 According to the traditions and customs of Uzbekistan, manta rays are served for lunch or dinner. Cooked manta rays are placed on the Lagan (large dish) and served to guests. Independently impose manta rays on a plate of the guest is not accepted: everyone takes as much as he wants to eat.
+               </p>
+               <p>
+                  <img lazy="loading" src="/image/blog_page/meals/manti2.jpg.jpg" alt="">
+               </p>
+               <p>
+                  Manta, like most of the Uzbek dishes, it is customary to eat with kushi. You can eat a dish with the help of Cutlery, but believe me… hands are more convenient and it seems that it tastes better.
+               </p>
+               <h3>What ingredients will you need to prepare a dish?</h3>
+               <p>
+                  To prepare manta, you will need ingredients for two components: dough and filling.
+                  <br>
+                  For the dough you will need:
+               </p>
+               <ul class="sumalak-ingredients">
+                   <li>1/2 kg flour;</li>
+                   <li>1-2 eggs;</li>
+                   <li>half a glass of boiled water;</li>
+                   <li>a teaspoon of salt;</li>
+                   <li>a tablespoon of vegetable oil.</li>
+               </ul>
+               <p>
+                  To prepare the filling is necessary:
+               </p>
+               <ul class="sumalak-ingredients">
+                   <li>1 kg of meat;</li>
+                   <li>100 g sheep’s tail fat (added on request);</li>
+                   <li>300 gr onion;</li>
+                   <li>a teaspoon of pepper;</li>
+                   <li>a teaspoon of cumin (cumin) or other meat spices.</li>
+               </ul>
+               <p>
+                  Dumplings can be cooked in different ways. For example, as a filling, you can use not only meat but also sliced potatoes with onions or potatoes with meat (in a proportion of 50/50). Some even manage to cook manta rays without meat and replace it with grated pumpkin.
+               </p>
+               <p>
+                 Real manta rays should be juicy. This is not difficult to achieve: add a lot of onions to the filling. The more onions, the juicier the dish
+               </p>
+               <h3>How to prepare mantas at home</h3>
+               <p>
+                 The process of preparation easy: prepare the ingredients, put the dumplings and cook them in the steamer. As in the preparation of any dish, some of the subtleties and secrets of cooking there.
+               </p>
+               <p>
+                  Prepare dough and ingredients
+               </p>
+               <p>
+                 If you have already prepared Uzbek flour products — it will be easier, because the dough is no different from what is prepared for other dishes.
+               </p>
+               <p>
+                  <img lazy="loading" src="/image/blog_page/meals/manti3.jpg" alt="">
+               </p>
+               <p>
+                 Now leave the dough “lie down” and prepare the filling. If you cook manta rays with meat, we recommend taking finely chopped pork or meat and grind it yourself through a meat grinder. Now add sliced onion, salt, pepper and other spices to the meat.
+               </p>
+               <h3>Sculpt the manta</h3>
+               <p>
+                 Some people think that it is difficult to sculpt manta rays. In fact, there is nothing complicated in cooking. Moreover, there are 2 ways of modeling: the first — to tell the dough and cut into squares of 8×10 cm, the second — to tell a few “sausages”, cut them into equal parts, and then roll out separately. The second method resembles the preparation of dumplings, only large.
+               </p>
+               <p>
+                 Next, apply a tablespoon of filling on top, add a piece of butter and fasten the ends. Of course, the first time you can not go out, but do not worry and try again. Take the opposite sides of the dough and begin to fasten them.
+               </p>
+               <p>
+                 Another tip for cooking. Take a special rolling pin and roll out the dough so that the edges are thinner than the middle. As a result, the seams on the finished manta rays will look not so rough
+               </p>
+               <h3>Cooking mantas</h3>
+               <p>
+                 In Central Asia use a special Martyshkino, and in Europe on the steamer. If you have manta-pot and steamers, don’t worry: you can take the colander to fit the size pot (not failed), and thus to prepare a dish.
+               </p>
+               <p>
+                 Here it’s not about the convenience of ways, and how many pieces you can cook at a time. For example, on Kantishna you can cook up to 40 pieces, but with the help of a colander — just a few pieces.
+               </p>
+               <p>
+                  <img lazy="loading" src="/image/blog_page/meals/manti4.jpg" alt="">
+               </p>
+               <p>
+                 First, grease the sheets of a double boiler (Martyshkino or colander) with oil, to dumplings are not stuck to the surface. Here, again, there is a second option: you can slightly dip the manta rays (in turn) in the oil and then put in a steamer. Cover the dish with a lid and cook for 40-45 minutes. Manta rays should be looked after and sprinkled with water when they are completely dry.
+               </p>
+               <h3>Serve on the table</h3>
+               <p>
+                 If you do not have a large flat plate, it does not matter: you can put manta rays in the plate of each member of the family. If desired, you can add sour cream, salt, herbs, and pepper.
+                 <br>
+                 The dish is incredibly tasty.
+               </p>
+               <blockquote><p>Manta rays are considered heavy food, so recommend not to hand.</p></blockquote>
+            </div>
+            
+            <? include_once './includes/form-input.php'; ?>
+        </div>
+    </div>
+</div>
